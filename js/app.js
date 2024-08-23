3ea51e921182652f46fb7948c4721cdae33b248e
bgonzalez <benjamin.gonzalez@ayi.group>
2024-08-23 14:48:51 -0300

    Agregado de clase AdministradorCitas.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (() => {
-    //---------------------------------------//
-    // CONSTANTES                            //
-    // --------------------------------------//
+    // ---------------------------------------------------------------------------- //
+    // CONSTANTES                                                                   //
+    // ---------------------------------------------------------------------------- //
     const ESTADO_VALIDACION = Object.freeze({
         EXITO: 'EXITO',
         ERROR: 'ERROR',
@@ -13,6 +13,8 @@
     });
     const CLASSLIST_ERROR = Object.freeze(['text-center', 'w-full', 'p-3',
         'text-white', 'my-5', 'alerta', 'uppercase', 'font-bold', 'text-sm']);
+    const CLASSLIST_DIV_CITAS = ['mx-5', 'my-10', 'bg-white', 'show-md', 'px-5', 'py-10', 'rounded-md'];
+    const CLASSLIST_PACIENTE = ['font-normal', 'mb-3', 'text-gray-700', 'normal-case'];
     const BACKGROUND_ERROR = 'bg-red-500';
     const BACKGROUND_EXITO = 'bg-green-500';
     const ESTADO_FORMULARIO = Object.freeze({
@@ -26,17 +28,121 @@
         HASHTAG: '#alerta-notificacion',
         SIN_HASHTAG: 'alerta-notificacion',
     });
-    const TIEMPO_ALERTA = 4500;
-    // --------------------------------------//
-    // INPUTS / FORMULARIO                   //
-    // --------------------------------------//
+    const TIEMPO_ALERTA = 2500;
+
+    // ---------------------------------------------------------------------------- //
+    // CLASES                                                                       //
+    // ---------------------------------------------------------------------------- //
+    class Notificacion {
+        constructor({ mensaje, tipo }) {
+            this.mensaje = mensaje;
+            this.tipo = tipo;
+        }
+        /**
+         * Muestra el mensaje con el que se instacio la clase.
+         */
+        mostrar() {
+            // creo el elemento
+            const notificacion = document.createElement('div');
+            notificacion.setAttribute('id', ID_NOTIFICACION.SIN_HASHTAG);
+            notificacion.classList.add(...CLASSLIST_ERROR);
+
+            // asigno el tipo de notificacion
+            this.tipo === ESTADO_VALIDACION.ERROR ?
+                notificacion.classList.add(BACKGROUND_ERROR) :
+                notificacion.classList.add(BACKGROUND_EXITO);
+            
+            // agrego mensaje
+            notificacion.textContent = this.mensaje;
+
+            // incercion de alerta:
+            // verifico que no exista la notificacion y la inserto
+            const alertaExistente = document.querySelector(ID_NOTIFICACION.HASHTAG);
+            alertaExistente?.remove();
+            formulario.parentElement.insertBefore(notificacion, formulario);
+
+            // callback para eliminar la notificacion
+            const cb = () => {
+                notificacion.remove();
+            };
+            // despues de 1.5 segundos, la elimino
+            setTimeout(cb, TIEMPO_ALERTA);
+        }
+    };
+
+    class AdministradorCitas {
+        constructor() {
+            this.citas = [];
+        }
+        /**
+         * Agrega una cita al arreglo.
+         * @param {ESTADO_FORMULARIO} cita 
+         */
+        agregar(cita) {
+            this.citas = [...this.citas, cita];
+            this.mostrar();
+        }
+        /**
+         * Muestra las citas o un mensaje por default.
+         */
+        mostrar() {
+            // limpiar HTML anterior.
+            while (citasContenedor.firstChild) {
+                citasContenedor.removeChild(citasContenedor.firstChild);
+            };
+
+            // generar citas
+            this.citas.forEach(cita => {
+                const divCita = document.createElement('DIV');
+                divCita.classList.add(...CLASSLIST_DIV_CITAS);
+                // rows:
+
+                //
+                const paciente = document.createElement('P');
+                paciente.classList.add(...CLASSLIST_PACIENTE);
+                paciente.innerHTML = `<span class="font-bold uppercase">Paciente: </span> ${cita && (cita.paciente ?? '-')}`
+                // propietario
+                const propietario = document.createElement('P');
+                propietario.classList.add(...CLASSLIST_PACIENTE);
+                propietario.innerHTML = `<span class="font-bold uppercase"> Propietario: </span> ${cita && (cita.propietario ?? '-')}`
+                // email
+                const email = document.createElement('P');
+                email.classList.add(...CLASSLIST_PACIENTE);
+                email.innerHTML = `<span class="font-bold uppercase">Contacto: </span> ${cita && (cita.email ?? '-')}`
+                // fechas
+                const fecha = document.createElement('P');
+                fecha.classList.add(...CLASSLIST_PACIENTE);
+                fecha.innerHTML = `<span class="font-bold uppercase">Fecha: </span> ${cita && (cita.fecha ?? '-')}`
+                // sintomas
+                const sintomas = document.createElement('P');
+                sintomas.classList.add(...CLASSLIST_PACIENTE);
+                sintomas.innerHTML = `<span class="font-bold uppercase">Sintomas: </span> ${cita && (cita.sintomas ?? '-')}`
+            
+                // insertar paciente
+                divCita.appendChild(paciente);
+                divCita.appendChild(propietario);
+                divCita.appendChild(email);
+                divCita.appendChild(fecha);
+                divCita.appendChild(sintomas);
+
+                // inserto las rows al contenedor
+                citasContenedor.appendChild(divCita);
+            });
+        }
+    };
+
+    // ---------------------------------------------------------------------------- //
+    // INPUTS / FORMULARIO                                                          //
+    // ---------------------------------------------------------------------------- //
     const inputPaciente = document.querySelector('#paciente');
     const inputPropietario = document.querySelector('#propietario');
     const inputEmail = document.querySelector('#email');
     const inputFecha = document.querySelector('#fecha');
     const inputSintomas = document.querySelector('#sintomas');
-
+    //formulario
     const formulario = document.querySelector('#formulario-cita');
+    // contenedor citas
+    const citasContenedor = document.querySelector('#citas');
     
     // Objecto de cita
     const citaValues = { ...ESTADO_FORMULARIO };
@@ -52,9 +158,11 @@
     // formulario?.addEventListener('change', escribirObjeto);
     formulario?.addEventListener('submit', enviarFormulario);
 
-    // --------------------------------------//
-    // FUNCIONES                             //
-    // --------------------------------------//
+    const citas = new AdministradorCitas();
+
+    // ---------------------------------------------------------------------------- //
+    // FUNCIONES                                                                    //
+    // ---------------------------------------------------------------------------- //
     /**
      * Escribe en la propiedad del formulario
      * @param {EventTarget} evento 
@@ -83,7 +191,8 @@
         notificacion.mostrar();
 
         if (respuesta.validacion === ESTADO_VALIDACION.EXITO) {
-            // se envian los datos al "backend".
+            // guardo la cita en el arreglo
+            citas.agregar(citaValues);
         }
     }
     /**
@@ -122,47 +231,5 @@
             validacion: ESTADO_VALIDACION.EXITO,
             mensaje: MENSAJES_VALIDACION.EXITOSO,
         };
-    }
-
-    // --------------------------------------//
-    // CLASES                                //
-    // --------------------------------------//
-    class Notificacion {
-        constructor({ mensaje, tipo }) {
-            this.mensaje = mensaje;
-            this.tipo = tipo;
-        }
-        /**
-         * Muestra el mensaje con el que se instacio la clase.
-         */
-        mostrar() {
-            // creo el elemento
-            const notificacion = document.createElement('div');
-            notificacion.setAttribute('id', ID_NOTIFICACION.SIN_HASHTAG);
-            notificacion.classList.add(...CLASSLIST_ERROR);
-
-            // asigno el tipo de notificacion
-            this.tipo === ESTADO_VALIDACION.ERROR ?
-                notificacion.classList.add(BACKGROUND_ERROR) :
-                notificacion.classList.add(BACKGROUND_EXITO);
-            
-            // agrego mensaje
-            notificacion.textContent = this.mensaje;
-
-            // incercion de alerta:
-            // verifico que no exista la notificacion y la inserto
-            const alertaExistente = document.querySelector(ID_NOTIFICACION.HASHTAG);
-            if (alertaExistente) {
-                alertaExistente.remove();
-            }
-            formulario.parentElement.insertBefore(notificacion, formulario);
-
-            // callback para eliminar la notificacion
-            const cb = () => {
-                notificacion.remove();
-            };
-            // despues de 1.5 segundos, la elimino
-            setTimeout(cb, TIEMPO_ALERTA);
-        }
     }
 })();
